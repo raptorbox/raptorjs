fc037e3af21750c55dedfddca1be27f1151b6772
Luca Capra <luca.capra@gmail.com>
2016-09-29 09:55:28 +0200

    added permission API test

---
+++ test/auth/permissions.test.js
@@ -0,0 +1,98 @@
+/*
+  global describe
+  global before
+  global it
+  global console
+*/
+
+var configFile = "../data/config.json";
+
+var Raptor = require('../../index');
+var Promise = require('bluebird');
+
+var assert = require('chai').assert;
+var d = require('debug')("raptorjs:test:auth:permissions");
+
+var json = require('../data/device');
+var r, user, object;
+
+var testSetPermission = function (newPerms) {
+  return object.permissions.set(user, newPerms)
+    .then(function (permissions) {
+      assert.isArray(permissions);
+      assert.equal(permissions.length, newPerms.length);
+      return Promise.resolve(permissions);
+    })
+    .then(function (perms) {
+      return object.permissions.get(user)
+        .then(function (permissions) {
+          d("User %s permissions %j", user.uuid, permissions);
+          assert.isArray(permissions);
+          assert.equal(permissions.length, perms.length);
+          return Promise.resolve();
+        });
+    })
+};
+
+describe('raptor auth service', function () {
+
+  before(function () {
+
+    r = new Raptor(require(configFile));
+    return r.create(json)
+      .then(function (obj) {
+        object = obj;
+        d("current user %s", r.currentUser().uuid);
+        d("created object %s", object.id);
+        return Promise.resolve(object);
+      })
+      .then(function (object) {
+        assert.isNotNull(object.id);
+        var name = "pippo" + (new Date()).getTime();
+        return r.auth.users.create({
+          username: name,
+          password: "foobar",
+          email: name + "@bar.local"
+        })
+          .then(function (usr) {
+            user = usr;
+            assert.isNotNull(user.uuid);
+            d("created new user %s", user.uuid);
+          });
+      });
+
+  });
+
+  describe('permissions API', function () {
+
+    it('should return empty list of permission', function () {
+
+      return object.permissions.get(user)
+        .then(function (permissions) {
+          d("User %s permissions %j", user.uuid, permissions);
+
+          assert.isArray(permissions);
+          assert.equal(permissions.length, 0);
+
+          return Promise.resolve();
+        });
+    });
+
+    it('should set new permissions', function () {
+      return testSetPermission(["read", "write"]);
+    });
+
+    it('should empty permissions', function () {
+      return testSetPermission([])
+    });
+
+    it('should fail for unknown permission', function () {
+      return testSetPermission(["foobar"]).catch(function(e) {
+        d(e);
+        return Promise.resolve();
+      })
+    });
+
+  });
+
+});
--- test/data/device.js
+++ test/data/device.js
@@ -1,31 +1,31 @@
 module.exports = {
-    "name": "smart sensor device",
-    "description": "test device",
-    "streams": {
-        "temperature": {
-            "description": "Measure the temperature",
-            "channels": {
-                "degree": {
-                    "type": "number",
-                },
-                "name": "string",
-                "color": "string",
-                "isActive": "boolean"
-            }
-        }
+  "name": "smart sensor device",
+  "description": "test device",
+  "streams": {
+    "temperature": {
+      "description": "Measure the temperature",
+      "channels": {
+        "degree": {
+          "type": "number",
+        },
+        "name": "string",
+        "color": "string",
+        "isActive": "boolean"
+      }
+    }
+  },
+  "customFields": {
+    "example": true
+  },
+  "settings": {
+    "storeEnabled": false
+  },
+  "actions": [
+    "makeCall",
+    {
+      "name": "makePhoto",
+      "status": "active=1&destroy=now"
     },
-    "customFields": {
-      "example": true
-    },
-    "settings": {
-      "storeEnabled": false
-    },
-    "actions": [
-      "makeCall",
-      {
-        "name": "makePhoto",
-        "status": "active=1&destroy=now"
-      },
-      "eatPotatoes"
-    ]
+    "eatPotatoes"
+  ]
 };
