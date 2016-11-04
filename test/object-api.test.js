14d16700dafb578ad16785ff8af70679fc7613bd
Luca Capra <luca.capra@gmail.com>
2016-11-04 08:22:22 +0100

    added test for obj delete

--- test/object-api.test.js
+++ test/object-api.test.js
@@ -7,59 +7,66 @@ var assert = require('chai').assert;
 var dbg = require('debug')("raptorjs:test:api");
 
 var json = require('./data/device');
-var raptor;
-var serviceObject;
+var raptor = new Raptor(require(configFile));
 
 describe('raptor', function () {
 
-  before(function () {
-    raptor = new Raptor(require(configFile));
-  });
-
-  describe('create device', function () {
+  describe('Manage object definition', function () {
     it('should create a new object', function () {
 
       //startup is pig slow :(
-      this.timeout(5000000);
+      this.timeout(5000);
 
       return raptor.create(json)
         .then(function (obj) {
-          serviceObject = obj;
           dbg("Created " + obj.id);
           assert.equal(json.name, obj.name);
           assert.notEqual(obj.id, null);
           return Promise.resolve();
         });
     });
-  });
 
-  describe('update device', function () {
     it('should update an object streams and actions', function () {
 
-      //startup is pig slow :(
-      this.timeout(5000000);
+      json.name = "update " + (new Date())
+      return raptor.create(json)
+        .then(function (serviceObject) {
 
-      if(!serviceObject) throw new Error("Object not loaded?");
-
-
-      serviceObject.actions = [];
-
-      serviceObject.setStreams([{
-        name: "mystream",
-        channels: {
-          test: 'number',
-          test1: 'string'
-        }
-      }]);
-
-      dbg("Updating object: %j", serviceObject.toJSON())
-      return serviceObject.update()
-        .then(function (obj) {
-          assert.equal(json.name, obj.name);
-          assert.notEqual(obj.id, null);
+          serviceObject.actions = [];
+          dbg("Current object streams: %j", serviceObject.toJSON().streams)
+          return serviceObject.setStreams([{
+            name: "mystream",
+            channels: {
+              test: 'number',
+              test1: 'string'
+            }
+          }])
+          .update()
+          .then(function (obj) {
+            dbg("Updated object streams: %j", obj.toJSON().streams)
+            assert.equal(json.name, obj.name);
+            assert.notEqual(obj.id, null);
+          })
         })
-
     });
-  });
 
-});
+    it('should delete an object', function () {
+
+      json.name = "delete " + (new Date())
+      return raptor.create(json)
+        .then(function (serviceObject) {
+          var id = serviceObject.id;
+          return serviceObject
+          .delete()
+          .then(function () {
+            dbg("Deleted object: %s", id)
+            return raptor.load(id).catch(function(err) {
+              assert.isTrue(err.message.match(/404 Not Found/i).length > 0)
+              return Promise.resolve()
+            })
+          })
+        })
+    });
+
+  }); //describe
+}); //describe
