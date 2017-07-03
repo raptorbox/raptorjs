dcf29a7bf46be2b2afeb9fcd893a9f53de410d6b
Luca Capra <luca.capra@gmail.com>
2017-07-03 10:23:09 +0200

    fixes

--- test/object-search.test.js
+++ test/object-search.test.js
@@ -14,26 +14,43 @@ var dbg = require('debug')("raptorjs:test:api");
 var json = require('./data/device');
 var raptor = new Raptor(require(configFile));
 
+let waitForIndex = function(len) {
+  dbg("Waiting %s for indexing", len)
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      dbg("Done waiting")
+      resolve()
+    }, len)
+  });
+}
+
 describe('raptor', function () {
 
   describe('search for objects', function () {
     it('should search for an object', function () {
 
       // startup may be slow :(
-      this.timeout(5000);
+      this.timeout(7500);
 
       return raptor.create(json)
         .then(function (obj) {
           dbg("Created %s (%s)", obj.id, obj.name);
-          return raptor.search(obj.name)
-            .then(function (list) {
-              dbg("Found records: %j", list);
-              assert.equal(list.map(o => o.id).indexOf(obj.id) > -1, true);
-            })
+          return waitForIndex(3500).then(function() {
+            return raptor.search(obj.name)
+          })
+          .then(function (list) {
+            list = list.map(o => o.id)
+            dbg("Found records: %j", list.length);
+            let idx = list.indexOf(obj.id)
+            dbg("found my object: %j", idx);
+            assert.isTrue(idx >= 0);
+          })
         });
     });
 
     it('should search for an object customField', function () {
+
+      this.timeout(7500);
 
       json.customFields = {
         foo: "bar100"
@@ -42,11 +59,13 @@ describe('raptor', function () {
       return raptor.create(json)
         .then(function (obj) {
           dbg("Created %s (%s)", obj.id, obj.name);
-          return raptor.search(obj.name)
-            .then(function (list) {
-              dbg("Found records: %j", list);
-              assert.equal(list.map(o => o.id).indexOf(obj.id) > -1, true);
-            })
+          return waitForIndex(3500).then(function() {
+            return raptor.search(obj.name)
+          })
+          .then(function (list) {
+            dbg("Found records: %s", list.length);
+            assert.equal(list.map(o => o.id).indexOf(obj.id) > -1, true);
+          })
         });
     });
 
