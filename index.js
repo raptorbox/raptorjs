bf5ec2b006349b4fec45990adc7992de3b7e3458
Luca Capra <luca.capra@gmail.com>
2017-11-04 07:54:36 +0100

    add client cred

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const d = require("debug")("raptorjs:index")
 const models = require("./lib/model/models")
 const EventEmitter = require("eventemitter3")
 
-const BASE_URL = "https://raptor.local"
+const BASE_URL = "http://raptor.local"
 
 /**
  * Raptor SDK wrapper
@@ -43,16 +43,13 @@ class Raptor extends EventEmitter {
         this.routes = require("./lib/routes")
         this.models = models
 
-        const defaultConfig = {
+        this.defaultConfig = {
             token: null,
             username: null,
             password: null,
             url: BASE_URL,
             debug: false,
             // oauth2
-            accessTokenUri: BASE_URL + "/oauth/access_token",
-            authorizationUri: BASE_URL + "/oauth/authorize",
-            redirectUri: BASE_URL + "/oauth/callback",
             clientId: null,
             clientSecret: null,
             scopes: null,
@@ -65,9 +62,7 @@ class Raptor extends EventEmitter {
             }
         }
 
-        this.config = Object.assign({}, defaultConfig, config)
-
-        d("Client configuration: %j", this.config)
+        this.setConfig(config)
     }
 
     getConfig() {
@@ -75,8 +70,20 @@ class Raptor extends EventEmitter {
     }
 
     setConfig(cfg) {
-        this.config = Object.assign({}, cfg)
+
+        this.config = Object.assign({}, this.defaultConfig, cfg)
+
+        // reset oauth2 url
+        if(this.config.clientId) {
+            this.config = Object.assign(this.config, {
+                accessTokenUri: this.config.accessTokenUri || `${this.config.url}${this.routes.accessTokenUri}`,
+                authorizationUri: this.config.authorizationUri || `${this.config.url}${this.routes.authorizationUri}`,
+                redirectUri: this.config.redirectUri || `${this.config.url}${this.routes.redirectUri}`,
+            })
+        }
+
         this.Auth().reset()
+        d("Client configuration: %j", this.config)
     }
 
     getClient() {
