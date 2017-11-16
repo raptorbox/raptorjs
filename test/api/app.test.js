b66bc6d87808e36ab3fa22668dd7eccc6d3ee49e
Luca Capra <luca.capra@gmail.com>
2017-11-16 21:22:03 +0100

    added app auth check

--- test/api/app.test.js
+++ test/api/app.test.js
@@ -4,6 +4,42 @@ const util = require("../util")
 const d = require("debug")("raptorjs:test:app")
 
 
+const createApp = () => {
+    return util.getRaptor().then((r) => {
+        return util.createUserInstance().then((usr1) => {
+            const usr1Id = usr1.Auth().getUser().id
+            return r.App().create({
+                name: util.randomName("app"),
+                users: [{
+                    id: usr1Id,
+                    roles: ["operator"]
+                }],
+                roles: [{
+                    name: "operator",
+                    permissions: [
+                        "admin_device",
+                        "read_app",
+                    ]
+                }]
+            }).then((app) => {
+                const rawdev = { name: util.randomName("dev"), domain: app.id }
+                return r.Inventory().create(rawdev).then((dev) => {
+                    d("Created dev %s owner %s", dev.id, dev.userId)
+                    return usr1.App().read(app).then((app) => {
+                        d("usr1 %s can read app %s", usr1Id, app.id)
+                        return usr1.Inventory().read(dev.id).then(() => {
+                            d("usr1 %s can read device %s", usr1Id, dev.id)
+                            return Promise.resolve({
+                                usr1, r, dev, app
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })    
+}
+
 describe("app service", function () {
 
     describe("App API", function () {
@@ -23,37 +59,15 @@ describe("app service", function () {
         })
 
         it("should read app", function () {
+            return createApp()
+        })
 
-            return util.getRaptor().then((r) => {
-                return util.createUserInstance().then((usr1) => {
-                    const usr1Id = usr1.Auth().getUser().id
-                    const rawdev = { name: util.randomName("dev") }
-                    return r.Inventory().create(rawdev).then((dev) => {
-                        return r.App().create({
-                            name: util.randomName("app"),
-                            users: [ {
-                                id: usr1Id,
-                                roles: [ "operator" ]
-                            } ],
-                            devices: [dev.id],
-                            roles: [{
-                                name: "operator",
-                                permissions: [
-                                    "admin_device",
-                                    "read_app",
-                                ]
-                            }]
-                        }).then((app) => {
-                            d("Created app %s", app.name)
-                            return usr1.App().read(app).then((app) => {
-                                d("usr1 can read app %s", app.name)
-                                return usr1.Inventory().read(app.devices[0]).then(() => {
-                                    return Promise.resolve()
-                                })
-                            })
-                        })
+        it("should not read tree", function () {
+            return createApp().then(({ r, usr1, app }) => {
+                return r.Tree().create({ name: util.randomName("tree"), domain: app.id })
+                    .then((tree) => {
+                        return usr1.Tree().read(tree.id).catch(() => Promise.resolve())
                     })
-                })
             })
         })
 
