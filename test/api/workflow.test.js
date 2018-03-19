470e9290f51f3149ec3cf1121bc0484ac20ed94d
lcapra <lcapra@fbk.eu>
2018-03-19 16:41:57 +0100

    initial workflow test support

--- test/api/workflow.test.js
+++ test/api/workflow.test.js
@@ -3,7 +3,19 @@ const assert = require("chai").assert
 // const d = require("debug")("raptorjs:test:workflow")
 const util = require("../util")
 
-const instanceName = "workflow_test" + Math.floor(Date.now() * Math.random())
+const newInstanceName = () => "workflow_test" + Math.floor(Date.now() * Math.random())
+
+
+const create = async(instanceName) => {
+
+    const r = await util.getRaptor()
+    await r.Workflow().create(instanceName)
+    const list = await r.Workflow().list()
+
+    assert.equal(1, list.filter(((n) => instanceName === n.Name)).length)
+
+    return Promise.resolve()
+}
 
 describe("Workflow", function () {
 
@@ -22,13 +34,34 @@ describe("Workflow", function () {
         return Promise.resolve()
     })
 
-    it("should create a workflow", async () => {
+    it("should create a workflow", () => create(newInstanceName()))
+
+    it("should get a workflow", async () => {
+
+        const instanceName = newInstanceName()
+        await create(instanceName)
 
         const r = await util.getRaptor()
-        await r.Workflow().create(instanceName)
-        const list = await r.Workflow().list()
 
-        assert.equal(1, list.filter(((n) => instanceName === n.Name)).length)
+        let instance = await r.Workflow().get(instanceName)
+        assert.equal(instance.Name, instanceName)
+
+        return Promise.resolve()
+    })
+
+    it("should restart a workflow", async () => {
+
+        const r = await util.getRaptor()
+        const instanceName = newInstanceName()
+        await create(instanceName)
+
+        await r.Workflow().stop(instanceName)
+        let instance = await r.Workflow().get(instanceName)
+        assert.equal(r.Workflow().states.stopped, instance.Status)
+
+        await r.Workflow().start(instanceName)
+        instance = await r.Workflow().get(instanceName)
+        assert.equal(r.Workflow().states.started, instance.Status)
 
         return Promise.resolve()
     })
